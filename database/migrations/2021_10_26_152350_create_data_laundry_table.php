64daf43ea0ce5991750afde97c19f24552514499
Satria Ananta <satriaanantagaming@gmail.com>
2021-10-26 22:33:06 +0700

    commit

---
+++ database/migrations/2021_10_26_152350_create_data_laundry_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDataLaundryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('data_laundry', function (Blueprint $table) {
+            $table->id();
+            $table->string('kode');
+            $table->date('tanggal');
+            $table->time('waktu', $precision = 0);
+            $table->float('berat', 8, 2);
+            $table->string('progres');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('data_laundry');
+    }
+}
