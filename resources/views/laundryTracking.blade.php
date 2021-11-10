917ffbfc45dcd184274ce04cd64618de24a29d07
Satria Ananta <satriaanantagaming@gmail.com>
2021-11-10 16:56:26 +0700

    commit

--- resources/views/laundryTracking.blade.php
+++ resources/views/laundryTracking.blade.php
@@ -212,54 +212,27 @@
                         <h1>Kode Tracking Laundry: {{ $result->kode}}</h1>
                         <div class="container">
                             <div class="timeline">
-                                <div class="row no-gutters justify-content-end justify-content-md-around align-items-start  timeline-nodes">
-                                    <div class="col-10 col-md-5 order-3 order-md-1 timeline-content">
-                                        <h3 class=" text-light">Timeline Heading</h3>
-                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe, eaque amet deleniti hic quas qui cumque delectus aliquid, eius quia quod, quae, aliquam aspernatur facilis. Minima quod corporis dignissimos porro.</p>
-                                    </div>
-                                    <div class="col-2 col-sm-1 px-md-3 order-2 timeline-image text-md-center">
-                                        <img src="img/img13.png" class="img-fluid" alt="img">
-                                    </div>
-                                    <div class="col-10 col-md-5 order-1 order-md-3 py-3 timeline-date">
-                                        <time>2018-02-23</time>
-                                    </div>
-                                </div>
-                                <div class="row no-gutters justify-content-end justify-content-md-around align-items-start  timeline-nodes">
-                                    <div class="col-10 col-md-5 order-3 order-md-1 timeline-content">
-                                        <h3 class=" text-light">Timeline Heading</h3>
-                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe, eaque amet deleniti hic quas qui cumque delectus aliquid, eius quia quod, quae, aliquam aspernatur facilis. Minima quod corporis dignissimos porro.</p>
-                                    </div>
-                                    <div class="col-2 col-sm-1 px-md-3 order-2 timeline-image text-md-center">
-                                        <img src="img/img13.png" class="img-fluid" alt="img">
-                                    </div>
-                                    <div class="col-10 col-md-5 order-1 order-md-3 py-3 timeline-date">
-                                        <time>2018-02-23</time>
-                                    </div>
-                                </div>
-                                <div class="row no-gutters justify-content-end justify-content-md-around align-items-start  timeline-nodes">
-                                    <div class="col-10 col-md-5 order-3 order-md-1 timeline-content">
-                                        <h3 class=" text-light">Timeline Heading</h3>
-                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe, eaque amet deleniti hic quas qui cumque delectus aliquid, eius quia quod, quae, aliquam aspernatur facilis. Minima quod corporis dignissimos porro.</p>
-                                    </div>
-                                    <div class="col-2 col-sm-1 px-md-3 order-2 timeline-image text-md-center">
-                                        <img src="img/img13.png" class="img-fluid" alt="img">
-                                    </div>
-                                    <div class="col-10 col-md-5 order-1 order-md-3 py-3 timeline-date">
-                                        <time>2018-02-23</time>
-                                    </div>
-                                </div>
-                                <div class="row no-gutters justify-content-end justify-content-md-around align-items-start  timeline-nodes">
-                                    <div class="col-10 col-md-5 order-3 order-md-1 timeline-content">
-                                        <h3 class=" text-light">Timeline Heading</h3>
-                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe, eaque amet deleniti hic quas qui cumque delectus aliquid, eius quia quod, quae, aliquam aspernatur facilis. Minima quod corporis dignissimos porro.</p>
-                                    </div>
-                                    <div class="col-2 col-sm-1 px-md-3 order-2 timeline-image text-md-center">
-                                        <img src="img/img13.png" class="img-fluid" alt="img">
-                                    </div>
-                                    <div class="col-10 col-md-5 order-1 order-md-3 py-3 timeline-date">
-                                        <time>2018-02-23</time>
-                                    </div>
-                                </div>
+                                @if ($result->progres==0)
+                                    @include('laundryStatus.status1')
+                                @elseif ($result->progres==1)
+                                    @include('laundryStatus.status1')
+                                    @include('laundryStatus.status2')
+                                @elseif ($result->progres==2)
+                                    @include('laundryStatus.status1')
+                                    @include('laundryStatus.status2')
+                                    @include('laundryStatus.status3')
+                                @elseif ($result->progres==3)
+                                    @include('laundryStatus.status1')
+                                    @include('laundryStatus.status2')
+                                    @include('laundryStatus.status3')
+                                    @include('laundryStatus.status4')
+                                @elseif ($result->progres==4)
+                                    @include('laundryStatus.status1')
+                                    @include('laundryStatus.status2')
+                                    @include('laundryStatus.status3')
+                                    @include('laundryStatus.status4')
+                                    @include('laundryStatus.status5')
+                                @endif
                             </div>
                         </div>
                     @endforeach
